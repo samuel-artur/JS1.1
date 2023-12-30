bc41a6fe32d966852fc7005ca1014336e853f2ca
samuel-artur <samuk3502@gmail.com>
2023-12-30 11:55:52 -0300

    excercício 12 atualizado

--- EX012.js
+++ EX012.js
@@ -1,4 +1,17 @@
 // velocidade máxxima de até 70
 // a cada 5km acima do limite ganha 1 ponto
 // Math.floor()
-// 
+// caso os pontos sejam maiores que 12 = "carteira suspensa"
+velocidademaxima(70.1)
+function velocidademaxima(velocidade){
+        if(velocidade <= 70){
+            console.log('Velocidade Permitida')
+        } else{
+            const pontos = Math.floor((velocidade - 70) / 5 )
+            if (pontos >= 12){
+                console.log('Carteira Suspensa')
+            } else{
+                console.log('Pontos - ' , pontos)
+            }
+        }
+}
