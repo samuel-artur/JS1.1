8d9d8e546eb6fe950f3af1b9fe73c29562eabef2
samuel-artur <samuk3502@gmail.com>
2024-01-06 20:24:09 -0300

    aula Math

---
+++ EX020.js
@@ -0,0 +1,18 @@
+// Math JavaScript
+
+// maior valor com math
+console.log(Math.max(5, 10, 0, 122, -15, 2))
+// menor valor com math
+console.log(Math.min(5, 10, 0, 122, -15, 2))
+// valor arrendondado com Math
+console.log(Math.round(5.10202))
+// valor positivo com Math
+console.log(Math.abs(-16))
+// raiz quadrada com Math
+console.log(Math.round(Math.sqrt(56)))
+// valor arrendondado para cima com Math
+console.log(Math.ceil(5.5))
+// valor arrendondado para baixo com Math
+console.log(Math.floor(5.5))
+// valor entre 0 e 1 com Math
+console.log(Math.random())
